4411ae75607797d4ab1d141b72a41ee0d874c561
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-03 14:09:44 +0100

    Accepted a post request in the home route

--- calculator.js
+++ calculator.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/index.html`)
 })
 
+app.post("/", (req, res) => {
+	res.send("Thanks for posting that!")
+})
+
 app.listen(3000, () => {
 	console.log("Server is running on port 3000")
 })
